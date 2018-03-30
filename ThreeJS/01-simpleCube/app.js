aa35dad80d4eb1abd2207fa9041fa26a3cb3330b
Peeyush Yadav <peeyushy95@gmail.com>
2018-03-30 14:35:47 +0530

    simple cube

---
+++ ThreeJS/01-simpleCube/app.js
@@ -0,0 +1,47 @@
+var cube = ( ()=>{
+    "use strict";
+
+    var scene = new THREE.Scene(),
+    renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
+    light = new THREE.AmbientLight(0xffffff),
+    camera,
+    box;
+
+    function initScene(){
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('webgl-container').appendChild(renderer.domElement);
+        scene.add(light);
+
+        camera = new THREE.PerspectiveCamera(
+                    35,
+                    window.innerWidth/window.innerHeight,
+                    1,
+                    1000
+                );
+
+        camera.position.z = 100;
+        scene.add(camera);
+
+        var geometry = new THREE.BoxGeometry( 20, 20, 20 );
+        var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+        cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        render();
+    }
+
+
+    function render(){
+        cube.rotation.y +=0.09;
+        cube.rotation.x +=0.02;
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    window.onload = initScene;
+
+    return {
+        scene : scene
+    }
+}
+)();
